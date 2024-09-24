2fc4ba82e26191daa8c563c99a19ef93ecace28d
Rutu Kansara <149906777+rutukansara@users.noreply.github.com>
2024-09-23 23:28:55 -0700

    Delete 0001-two-sum directory

--- 0001-two-sum/0001-two-sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-        for i, n in enumerate(nums):
-            number = target - n
-            if number in hashmap:
-                return [hashmap[number], i]
-            else:
-                hashmap[n] = i
